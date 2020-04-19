8abe45cfffff88e902613a688a1d3d9f00467c43
hash2666 <huuut2544@gmail.com>
2020-04-19 21:54:47 +0900

    slider

---
+++ swiper.js
@@ -0,0 +1,36 @@
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  // direction: 'horizontal',
+  // loop: true,
+  effect: 'coverflow',
+  slidesPerView: 2.5,
+  speed:800,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type:'bullets',
+    clickable: true,
+    dynamicBullets:'span',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  autoplay: {
+		delay: 500,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+  fadeEffect: {
+    crossFade: true
+  },
+})
